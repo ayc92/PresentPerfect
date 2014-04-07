2805807cafb2ac69b1149d8d5530a843ffc8f154
Sergio Macias <sergioam1992@gmail.com>
2014-04-07 07:48:43 -0700

    Testing commit and push.

--- src/com/radishugrads/presentperfect/MotherBrain.java
+++ src/com/radishugrads/presentperfect/MotherBrain.java
@@ -1,7 +1,8 @@
 package com.radishugrads.presentperfect;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
 
 public class MotherBrain extends Activity {
@@ -10,6 +11,7 @@ public class MotherBrain extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_mother_brain);
+		//Test
 	}
 
 	@Override
