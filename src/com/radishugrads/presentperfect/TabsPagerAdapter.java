664b9f88088726681375f526d689416daaa29d97
Andrew Chen <aychen92@gmail.com>
2014-05-06 15:08:44 -0700

    always go back to pitch list tab

--- src/com/radishugrads/presentperfect/TabsPagerAdapter.java
+++ src/com/radishugrads/presentperfect/TabsPagerAdapter.java
@@ -19,7 +19,6 @@ public class TabsPagerAdapter extends FragmentPagerAdapter {
 
 	@Override
 	public Fragment getItem(int index) {
-		System.out.println("called this");
 		// only the notifications tab is not an expandable list view
 		if (index == 0) {
 			TabList tbs = TabList.newInstance("notifications");
